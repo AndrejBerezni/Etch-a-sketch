acd83fdf87397463a59d26259144df7b31e77be6
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-03-13 13:35:46 +0000

    optimize resizing function

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ const sizeIndicator = document.getElementById('size-indicator');
 
 sizeIndicator.innerText = `${size.value} x ${size.value}`
 
-let initializeGrid = () => {
+function initializeGrid () {
+    grid.style.gridTemplateColumns = `repeat(${size.value}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${size.value}, 1fr)`;
+        sizeIndicator.innerText = `${size.value} x ${size.value}`;
     for (let i = 0; i < size.value * size.value; i++) {
-        let gridUnit = document.createElement('div');
-        gridUnit.style.height = `${grid.clientHeight / size.value}`;
-        gridUnit.style.width = `${grid.clientWidth / size.value}`;
-        // gridUnit.style.border = '1px solid black';
-        grid.style.gridTemplateColumns = `repeat(${size.value}, 1fr)`
-        grid.style.gridTemplateRows = `repeat(${size.value}, 1fr)`
-        sizeIndicator.innerText = `${size.value} x ${size.value}`
+        let gridUnit = document.createElement('div');         
         grid.appendChild(gridUnit);
     }
 }
